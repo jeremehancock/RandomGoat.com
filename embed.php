abbb637d11571f2301973f2c98574490de25a3f5
Jereme <michael@jeremehancock.com>
2025-06-25 13:00:36 -0500

    initial commit

---
+++ embed.php
@@ -0,0 +1,33 @@
+<?php
+// Simple embed.php file to iframe index.html full screen
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Embedded Content</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        
+        html, body {
+            height: 100%;
+            overflow: hidden;
+        }
+        
+        iframe {
+            width: 100%;
+            height: 100%;
+            border: none;
+            display: block;
+        }
+    </style>
+</head>
+<body>
+    <iframe src="index.html" frameborder="0" allowfullscreen></iframe>
+</body>
+</html>
