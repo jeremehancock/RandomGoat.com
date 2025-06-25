487c5eac37a5956abca8e0f7aff43207254a08cc
Jereme <michael@jeremehancock.com>
2025-06-25 13:23:03 -0500

    added statcounter

--- embed.php
+++ embed.php
@@ -29,5 +29,23 @@
 </head>
 <body>
     <iframe src="index.html" frameborder="0" allowfullscreen></iframe>
+    
+    <!-- Default Statcounter code for Random Goat v2 Embed https://randomgoat.com/embed.php -->
+	<script type="text/javascript">
+	var sc_project=13146733; 
+	var sc_invisible=1; 
+	var sc_security="2b8ffaa1"; 
+	</script>
+	<script type="text/javascript"
+	src="https://www.statcounter.com/counter/counter.js"
+	async></script>
+	<noscript><div class="statcounter"><a title="Web Analytics
+	Made Easy - Statcounter" href="https://statcounter.com/"
+	target="_blank"><img class="statcounter"
+	src="https://c.statcounter.com/13146733/0/2b8ffaa1/1/"
+	alt="Web Analytics Made Easy - Statcounter"
+	referrerPolicy="no-referrer-when-downgrade"></a></div></noscript>
+	<!-- End of Statcounter Code -->
+	
 </body>
 </html>
